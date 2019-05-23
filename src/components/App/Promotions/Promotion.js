3d7998c221679f4c7e03abfb913800c320dc55f0
ThearyIm <thearyim@gmail.com>
2019-05-22 18:31:36 -0700

    Add event details componenet

--- src/components/App/Promotions/Promotion.js
+++ src/components/App/Promotions/Promotion.js
@@ -3,20 +3,31 @@ import SiteUtils from '_SiteJs';
 import MockData from '_MockDataJs';
 import './Promotion.css';
 import Showdown from 'showdown/dist/showdown.js';
+import casey from '_SiteImages/Finished_Flyer_page-0001_large.jpg';
 
 const Promotion = ({ info }) => {
 
   var mockData = new MockData();
   var converter = new Showdown.Converter();
-  var htmlDescription = convertMarkdownToHtml(info.description);
+  // var htmlDescription = convertMarkdownToHtml(info.description);
 
   function convertMarkdownToHtml (markdown) {
     return converter.makeHtml(markdown);
   }
 
+  // <div dangerouslySetInnerHTML={{ __html: htmlDescription }} />
+  var gridStyle = {
+    display: 'grid',
+    gridTemplateColumns: '40% 60%'
+  }
+
   return(
-    <div className="container promotion">
-      <div dangerouslySetInnerHTML={{ __html: htmlDescription }} />
+    <div className="container promotion" style={gridStyle}>
+      <div><img src={casey} /></div>
+      <div className="promotion-description">
+        <div className="description-name">{info.name}</div>
+        <div className="description-desc">{info.description.text}</div>
+      </div>
     </div>
   );
 }
--- src/js/mockData.js
+++ src/js/mockData.js
@@ -282,8 +282,12 @@ class MockData {
     return [
       {
         id: 1,
-        name: 'Win a Chance to Play with the pro!',
-        description: '###Win a Chance to Play with the pro! \n\n **Support Uprising Volleyball by Donating Today.**  \n  Support Uprising Volleyball with a donation between April 11 and May 23 and you will be entered to win a chance to play with The Lorax, [Jeremy Casebeer at Uprising Volleyball\'s Play with the Pro event](https://someevent) from 6-8pm at Willis Tucker Park in Snohomish.',
+        name: 'Unified Sport Beach Volleyball Clinic with Jeremy Casebeer',
+        description: {
+          text: 'Tomorrow Warriors has organized the first beach volleyball Unified Sport clinic at Uprising Volleyball.',
+          image: 'Finished_Flyer_page-0001_large.jpg'
+        },
+        detail: '# Unified Sport Beach Volleyball Clinic with Jeremy Casebeer  \n Tomorrow Warriors has organized the first beach volleyball Unified Sport clinic at Uprising Volleyball.   Athletes with and without disabilities will have the opportunity to work with AVP and Team USA pro beach volleyball player Jeremy Casebeer to learn the basic skills of beach volleyball. Special thanks to [Five12 Apparel](https://five12apparel.com/), [AVPFirst](http://avpfirst.com/), North Sound Physical Therapy and [Ink It Your Way](http://www.inkityourway.com/) for making this clinics possible.  \n\nPlease say thanks to them for making this work. What is Unified Sport? Dedicated to promoting social inclusion through shared sports training and competition experiences, Unified Sports joins people with and without intellectual disabilities on the same team. It was inspired by a simple principle: training together and playing together is a quick path to friendship and understanding. In Unified Sports, teams are made up of people of similar age and ability, which makes practices more fun and games more challenging and exciting for all. Having sport in common is just one more way that preconceptions and false ideas are swept away. Our opponent is intolerance. Only shoulder-to-shoulder, as teammates together, can we defeat it.  [Learn More About Unified Sport Here](http://www.playunified.org/about).  \n\nOne of the top up and coming blockers on the AVP Tour and internationally with USA Volleyball,  Jeremy Casebeer is an active and outspoken advocate for the environment and human rights.   He uses his talent on the volleyball court to create a platform for social change.   [Learn more about Jeremy Here](http://jeremycasebeer.com/?page_id=88). Questions?  Email UprisingVolleyball@gmail.com or call Mati at 425-344-0408.',
         dates: [
           {
             description: 'promotionEnds',
@@ -295,7 +299,11 @@ class MockData {
       {
         id: 2,
         name: 'Win a Chance to Play with the pro!',
-        description: '###Win a Chance to Play with the pro! \n\n **Support Uprising Volleyball by Donating Today.**  \n  Support Uprising Volleyball with a donation between April 11 and May 23 and you will be entered to win a chance to play with The Lorax, [Jeremy Casebeer at Uprising Volleyball\'s Play with the Pro event](https://someevent) from 6-8pm at Willis Tucker Park in Snohomish.',
+        description: {
+          text: 'Tomorrow Warriors has organized the first beach volleyball Unified Sport clinic at Uprising Volleyball.',
+          image: ''
+        },
+        detail: '# Unified Sport Beach Volleyball Clinic with Jeremy Casebeer  \n Tomorrow Warriors has organized the first beach volleyball Unified Sport clinic at Uprising Volleyball.   Athletes with and without disabilities will have the opportunity to work with AVP and Team USA pro beach volleyball player Jeremy Casebeer to learn the basic skills of beach volleyball. Special thanks to [Five12 Apparel](https://five12apparel.com/), [AVPFirst](http://avpfirst.com/), North Sound Physical Therapy and [Ink It Your Way](http://www.inkityourway.com/) for making this clinics possible.   Please say thanks to them for making this work. What is Unified Sport? Dedicated to promoting social inclusion through shared sports training and competition experiences, Unified Sports joins people with and without intellectual disabilities on the same team. It was inspired by a simple principle: training together and playing together is a quick path to friendship and understanding. In Unified Sports, teams are made up of people of similar age and ability, which makes practices more fun and games more challenging and exciting for all. Having sport in common is just one more way that preconceptions and false ideas are swept away. Our opponent is intolerance. Only shoulder-to-shoulder, as teammates together, can we defeat it.  [Learn More About Unified Sport Here](http://www.playunified.org/about). One of the top up and coming blockers on the AVP Tour and internationally with USA Volleyball,  Jeremy Casebeer is an active and outspoken advocate for the environment and human rights.   He uses his talent on the volleyball court to create a platform for social change.   [Learn more about Jeremy Here](http://jeremycasebeer.com/?page_id=88). Questions?  Email UprisingVolleyball@gmail.com or call Mati at 425-344-0408.',
         dates: [
           {
             description: 'promotionEnds',
